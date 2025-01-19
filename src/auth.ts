af689cf3a46286b4e2597f1acb683ed357e21bbf
contigen <69436315+contigen@users.noreply.github.com>
2025-01-19 20:04:29 +0100

    Refactor auth config

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,11 @@
-import { loginSchema } from './lib/schema'
+import { loginSchema } from '&/lib/schema'
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import Google from 'next-auth/providers/google'
 import LinkedIn from 'next-auth/providers/linkedin'
-import { createUser, getUser } from '&/lib/db-queries'
+import { getUser } from '&/lib/db-queries'
 import { compare } from 'bcrypt-ts'
 import { NextResponse } from 'next/server'
-import prisma from './lib/prisma'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -35,13 +34,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   callbacks: {
     async authorized({ request: req, auth }) {
-      const PUBLIC_ROUTES = [
-        `/`,
-        `/login`,
-        `/signup`,
-        `/forgot-password`,
-        `/candidate/profile`,
-      ]
+      const PUBLIC_ROUTES = [`/`, `/login`, `/register`]
       const { pathname } = req.nextUrl
       const isLoggedIn = !!auth?.user
       const isAPublicRoute = PUBLIC_ROUTES.some(route => route === pathname)
@@ -64,61 +57,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async session({ session, token }) {
       session.user.id = token.id as string
       return session
-    },
-    async signIn({ user, account }) {
-      const provider = account?.provider
-      const providerAccountId = account?.providerAccountId
-
-      if (provider !== `credentials`) {
-        if (!provider || !providerAccountId) {
-          console.error(`Missing provider or providerAccountId`)
-          return false
-        }
-
-        const { email, name } = user
-        if (!email || !name) {
-          console.error(`Missing email or name from OAuth provider`)
-          return false
-        }
-
-        try {
-          const userFromDB = await getUser(email)
-          if (!userFromDB) {
-            const newUser = await createUser({
-              name,
-              email,
-              providerDetails: { provider, providerAccountId },
-            })
-            if (!newUser) {
-              return false
-            }
-            return true
-          }
-
-          const linkedAccount = await prisma.authAccount.findFirst({
-            where: {
-              provider,
-              providerAccountId,
-            },
-          })
-
-          if (!linkedAccount) {
-            await prisma.authAccount.create({
-              data: {
-                provider,
-                providerAccountId,
-                userId: userFromDB.id,
-              },
-            })
-          }
-
-          return true
-        } catch {
-          return false
-        }
-      }
-
-      return true
     },
   },
   pages: {
