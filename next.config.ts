2c0b5c1130b4d908a33b1b8e077200ff2c006144
contigen <69436315+contigen@users.noreply.github.com>
2025-01-29 00:11:25 +0100

    Update  next config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,22 @@ const nextConfig: NextConfig = {
       bodySizeLimit: `3mb`,
     },
   },
+  images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: `https`,
+    //     hostname: `nft-reverse-image-search.s3.eu-central-1.amazonaws.com`,
+    //     port: ``,
+    //     search: ``,
+    //   },
+    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
